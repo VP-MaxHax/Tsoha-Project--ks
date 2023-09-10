eeff5e78a2d7861882c1aad772bbd02808a5809b
VP-MaxHax <vp.juselius@gmail.com>
2023-09-10 19:00:25 +0300

    Added linking between psql and flask

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    result = db.session.execute(text("SELECT content FROM messages"))
+    messages = result.fetchall()
+    #raise Exception(messages)
+    return render_template("index.html", count=len(messages), messages=messages) 
+
+@app.route("/login")
+def login():
+    return "Login page"
+
+@app.route("/register")
+def register():
+    return "Register page"
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\VP\Documents\Omat_opiskelut\Tsoha\venv
+set VIRTUAL_ENV=C:\Users\VP\OneDrive - University of Helsinki\Omat_opiskelut\Tsoha\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
