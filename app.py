c14defcfe1b5065eea6918b90052ce787e842b08
VP-MaxHax <vp.juselius@gmail.com>
2023-09-18 12:06:02 +0300

    Added user registering/login fuctions

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = "a4540e650c519ab55774ce2e5aad57b3"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///postgres"
 db = SQLAlchemy(app)
 
@@ -11,13 +14,49 @@ db = SQLAlchemy(app)
 def index():
     result = db.session.execute(text("SELECT content FROM messages"))
     messages = result.fetchall()
-    #raise Exception(messages)
     return render_template("index.html", count=len(messages), messages=messages) 
 
 @app.route("/login")
 def login():
-    return "Login page"
+    return render_template("login.html")
+
+@app.route("/loginuser",methods=["POST"])
+def login_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    sql = text("SELECT user_id, password FROM users WHERE username=:username")
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()  
+    if not user:
+        raise Exception("Wrong username")
+    else:
+        hash_value = user.password
+        if check_password_hash(hash_value, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return redirect("/login")
 
 @app.route("/register")
 def register():
-    return "Register page"
+    return render_template("/register.html")
+
+@app.route("/registeruser", methods=["POST"])
+def register_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username and password:
+        sql = text("INSERT INTO users (username, password, is_sub, sub_exp, is_active, is_staff, last_login) " \
+                   "VALUES (:username, :password, :is_sub, :sub_exp, :is_active, :is_staff, :last_login);")
+        time = datetime.now()
+        db.session.execute(sql, {"username":username, "password":generate_password_hash(password),"is_sub":"f", \
+                                 "sub_exp":time, "is_active":"t", "is_staff":"f", "last_login":time})
+        db.session.commit()
+        session["username"] = username
+        return redirect("/")
+    return redirect("/register")
+
+@app.route("/logout")
+def logout():
+    session["username"] = None
+    return redirect("/")
