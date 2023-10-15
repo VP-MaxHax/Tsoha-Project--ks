6b0c7a3703f544667d7b7e97f639964e00f673a0
VP-MaxHax <vp.juselius@gmail.com>
2023-10-15 07:07:43 +0300

    Moved database schema from info.txt to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,37 @@
 CREATE TABLE visitors (id SERIAL PRIMARY KEY, time TIMESTAMP);
+
+Users table:
+CREATE TABLE users (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR NOT NULL,
+	is_sub BOOLEAN NOT NULL,
+	sub_exp TIMESTAMP,
+	is_active BOOLEAN NOT NULL,
+	is_staff BOOLEAN NOT NULL,
+    last_login TIMESTAMP 
+);
+
+Messages table:
+CREATE TABLE messages (
+	id serial PRIMARY KEY,
+	content VARCHAR ( 100 ),
+	posted_by INTEGER,
+	hidden BOOLEAN
+);
+
+Comments table:
+CREATE TABLE comments (
+	comment_id serial PRIMARY KEY,
+	content VARCHAR ( 100 ),
+	source_msg INTEGER,
+	posted_by VARCHAR,
+	hidden BOOLEAN
+);
+
+follows table:
+CREATE TABLE follows (
+	id serial PRIMARY KEY,
+	user_id INTEGER NOT NULL,
+	following_id INTEGER NOT NULL
+);
