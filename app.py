8af8413bf3bb8973672267633e70a77be61760e1
VP-MaxHax <vp.juselius@gmail.com>
2023-10-22 13:23:24 +0300

    Did some code cleaning with the help of pylint

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
-from flask import redirect, render_template, request, session
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime, timedelta
 from os import getenv
-from db import get_userid, get_username, get_messages, get_latest, get_followed, get_comments, db, get_membership, get_clubmessages, log_user
 from secrets import token_hex
+from werkzeug.security import check_password_hash, generate_password_hash
+from sqlalchemy.sql import text
+from flask import Flask, redirect, render_template, request, session
+from db import db, get_userid, get_username, get_messages, get_clubmessages,\
+      get_latest, get_followed, get_comments, get_membership, log_user
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
@@ -53,14 +52,15 @@ def messages_followed():
     return render_template("followed.html", count=count[0], messages=messages)
 
 #Page shows detailed info on message
-@app.route("/messages/<int:id>")
-def detailed(id:int):
+@app.route("/messages/<int:page_id>")
+def detailed(page_id: int):
     sql = text("SELECT id, content, posted_by FROM messages WHERE id=:id")
-    result = db.session.execute(sql, {"id":id})
+    result = db.session.execute(sql, {"id":page_id})
     message = result.fetchone()
     username = get_username(message[2])
-    comments = get_comments(id)
-    return render_template("detailed.html", message=message, comments=comments, username=username, user_id=message[2])
+    comments = get_comments(page_id)
+    return render_template("detailed.html", message=message, comments=comments,\
+                            username=username, user_id=message[2])
 
 #Page that allows new messages to be posted
 @app.route("/messages/new")
@@ -79,8 +79,10 @@ def newmessage():
         return render_template("error.html", error="Error 403. Forbidden.")
     if content:
         user = get_userid()
-        sql = text("INSERT INTO messages (content, posted_by, hidden, is_for_members) VALUES (:content, :posted_by, :hidden :is_for_members);")
-        db.session.execute(sql, {"content":content, "posted_by":user, "hidden":"f", "is_for_members":is_for_members})
+        sql = text("INSERT INTO messages (content, posted_by, hidden, is_for_members)\
+                    VALUES (:content, :posted_by, :hidden :is_for_members);")
+        db.session.execute(sql, {"content":content, "posted_by":user, "hidden":"f",\
+                                  "is_for_members":is_for_members})
         db.session.commit()
     return redirect("/messages")
 
@@ -98,16 +100,14 @@ def login_user():
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if not user:
-        raise Exception("Wrong username")
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-            session["username"] = username
-            session["csrf_token"] = token_hex(16)
-            log_user("login")
-            return redirect("/")
-        else:
-            return redirect("/login")
+        return redirect("/login")
+    hash_value = user.password
+    if check_password_hash(hash_value, password):
+        session["username"] = username
+        session["csrf_token"] = token_hex(16)
+        log_user("login")
+        return redirect("/")
+    return redirect("/login")
 
 #New user can be registered on this page
 @app.route("/register")
@@ -120,11 +120,15 @@ def register_user():
     username = request.form["username"]
     password = request.form["password"]
     if username and password:
-        sql = text("INSERT INTO users (username, password, is_sub, sub_exp, is_active, is_staff, last_login) " \
-                   "VALUES (:username, :password, :is_sub, :sub_exp, :is_active, :is_staff, :last_login);")
+        sql = text("INSERT INTO users (username, password, is_sub,\
+                    sub_exp, is_active, is_staff, last_login)\
+                    VALUES (:username, :password, :is_sub, :sub_exp,\
+                    :is_active, :is_staff, :last_login);")
         time = datetime.now()
-        db.session.execute(sql, {"username":username, "password":generate_password_hash(password),"is_sub":"f", \
-                                 "sub_exp":time, "is_active":"t", "is_staff":"f", "last_login":time})
+        db.session.execute(sql, {"username":username,\
+                                  "password":generate_password_hash(password),\
+                                    "is_sub":"f", "sub_exp":time, "is_active":"t",\
+                                        "is_staff":"f", "last_login":time})
         db.session.commit()
         session["username"] = username
         session["csrf_token"] = token_hex(16)
@@ -144,7 +148,7 @@ def logout():
 @app.route("/newcomment", methods=["POST"])
 def new_comment():
     content = request.form["comment"]
-    id = request.form["id"]
+    message_id = request.form["id"]
     user = session["username"]
     if not user:
         user = "Anonymous"
@@ -154,53 +158,55 @@ def new_comment():
         if session["csrf_token"] != request.form["csrf_token"]:
             return render_template("error.html", error="Error 403. Forbidden.")
     if content:
-        sql = text("INSERT INTO comments (content, source_msg, posted_by, poster_id, hidden) VALUES (:content, :source_msg, :posted_by, :poster_id, :hidden);")
-        db.session.execute(sql, {"content":content, "source_msg":id, "posted_by":user, "poster_id":poster_id, "hidden":"f"})
+        sql = text("INSERT INTO comments (content, source_msg, posted_by, poster_id, hidden)\
+                    VALUES (:content, :source_msg, :posted_by, :poster_id, :hidden);")
+        db.session.execute(sql, {"content":content, "source_msg":message_id,\
+                                  "posted_by":user, "poster_id":poster_id, "hidden":"f"})
         db.session.commit()
-    return redirect(f"/messages/{id}")
+    return redirect(f"/messages/{message_id}")
 
 #Adds followed information to database
 @app.route("/followuser", methods=["POST"])
 def follow_user():
     user = get_userid()
-    id = request.form["id"]
+    following_id = request.form["id"]
     if session["csrf_token"] != request.form["csrf_token"]:
         return render_template("error.html", error="Error 403. Forbidden.")
     if user:
         sql = text("INSERT INTO follows (user_id, following_id) VALUES (:user_id, :following_id);")
-        db.session.execute(sql, {"user_id":user, "following_id":id})
+        db.session.execute(sql, {"user_id":user, "following_id":following_id})
         db.session.commit()
-        return redirect(f"/user/{id}")
+        return redirect(f"/user/{following_id}")
     return render_template("error.html", error="Must be logged in to follow users!")
 
 #Removes followed information from database
 @app.route("/unfollowuser", methods=["POST"])
 def unfollow_user():
     user = get_userid()
-    id = request.form["id"]
+    following_id = request.form["id"]
     if session["csrf_token"] != request.form["csrf_token"]:
         return render_template("error.html", error="Error 403. Forbidden.")
     if user:
         sql = text("DELETE FROM follows WHERE user_id=:user_id AND following_id=:following_id;")
-        db.session.execute(sql, {"user_id":user, "following_id":id})
+        db.session.execute(sql, {"user_id":user, "following_id":following_id})
         db.session.commit()
-        return redirect(f"/user/{id}")
+        return redirect(f"/user/{following_id}")
     return render_template("error.html", error="Must be logged in to follow users!")
 
 #Page detailing information of specific users
-@app.route("/user/<int:id>")
-def user_details(id:int):
+@app.route("/user/<int:page_id>")
+def user_details(page_id: int):
     sql = text("SELECT username, last_login FROM users WHERE user_id=:id")
-    result = db.session.execute(sql, {"id":id})
+    result = db.session.execute(sql, {"id":page_id})
     user = result.fetchone()
     sql = text("SELECT following_id FROM follows WHERE user_id=user_id")
     result = db.session.execute(sql, {"user_id":get_userid()})
     following = result.fetchall()
     is_following = False
     for i in following:
-        if i[0]==id:
+        if i[0]==page_id:
             is_following = True
-    return render_template("user.html", user = user, following = is_following, page_id = id)
+    return render_template("user.html", user = user, following = is_following, page_id = page_id)
 
 #Page for getting Äks-club memberships
 @app.route("/membership")
@@ -230,5 +236,5 @@ def aks_club():
     if user and get_membership():
         messages, count = get_clubmessages()
         return render_template("club.html", count=count[0], messages=messages)
-    return render_template("error.html", error="You have to be logged in and a Äks-Club member to access this page.")
-
+    msg = "You have to be logged in and a Äks-Club member to access this page."
+    return render_template("error.html", error=msg)
--- db.py
+++ db.py
@@ -1,21 +1,21 @@
-from flask import Flask
-from flask import redirect, render_template, request, session
+from datetime import datetime
+from flask import render_template, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
 
 db = SQLAlchemy()
 
 #Fetch user id based on active session username
 def get_userid():
-    sql = text("SELECT user_id FROM users WHERE username=:username")
-    result = db.session.execute(sql, {"username":session["username"]})
-    user = result.fetchone()
-    return user[0]
+    if session["username"]:
+        sql = text("SELECT user_id FROM users WHERE username=:username")
+        result = db.session.execute(sql, {"username":session["username"]})
+        user = result.fetchone()
+        return user[0]
+    return None
 
 #Fetch username based on user id.
-def get_username(user_id:int):
+def get_username(user_id: int):
     sql = text("SELECT username FROM users WHERE user_id=:user_id")
     result = db.session.execute(sql, {"user_id":user_id})
     username = result.fetchone()
@@ -23,29 +23,36 @@ def get_username(user_id:int):
 
 #Fetch all messages on messages table
 def get_messages():
-    if get_membership:
+    if get_membership():
         result = db.session.execute(text("SELECT id, content FROM messages"))
         count = db.session.execute(text("SELECT COUNT(*) FROM messages"))
     else:
-        result = db.session.execute(text("SELECT id, content FROM messages WHERE is_for_members=False"))
-        count = db.session.execute(text("SELECT COUNT(*) FROM messages WHERE is_for_members=False"))
+        result = db.session.execute(text("SELECT id, content FROM messages\
+                                          WHERE is_for_members=False"))
+        count = db.session.execute(text("SELECT COUNT(*) FROM messages\
+                                         WHERE is_for_members=False"))
     messages = result.fetchall()
     amount = count.fetchone()
     return messages, amount
 
+#Fetch all members only messages
 def get_clubmessages():
-    result = db.session.execute(text("SELECT id, content FROM messages WHERE is_for_members=True"))
-    count = db.session.execute(text("SELECT COUNT(*) FROM messages WHERE is_for_members=True"))
+    result = db.session.execute(text("SELECT id, content FROM messages\
+                                      WHERE is_for_members=True"))
+    count = db.session.execute(text("SELECT COUNT(*) FROM messages\
+                                     WHERE is_for_members=True"))
     messages = result.fetchall()
     amount = count.fetchone()
     return messages, amount
 
 #Fetch latest five messages from database
 def get_latest():
-    if get_membership:
-        result = db.session.execute(text("SELECT id, content FROM messages ORDER BY id DESC LIMIT 5"))
+    if get_membership():
+        result = db.session.execute(text("SELECT id, content FROM messages\
+                                          ORDER BY id DESC LIMIT 5"))
     else:
-        result = db.session.execute(text("SELECT id, content FROM messages WHERE is_for_members=False ORDER BY id DESC LIMIT 5"))
+        result = db.session.execute(text("SELECT id, content FROM messages\
+                                          WHERE is_for_members=False ORDER BY id DESC LIMIT 5"))
     messages = result.fetchall()
     return messages
 
@@ -62,20 +69,26 @@ def get_followed():
             helplist.append(str(i[0]))
         following = " OR posted_by=".join(helplist)
         if sub_status:
-            result = db.session.execute(text(f"SELECT id, content FROM messages WHERE posted_by={following}"))
-            count = db.session.execute(text(f"SELECT COUNT(*) FROM messages WHERE posted_by={following}"))
+            result = db.session.execute(text(f"SELECT id, content FROM messages\
+                                              WHERE posted_by={following}"))
+            count = db.session.execute(text(f"SELECT COUNT(*) FROM messages\
+                                             WHERE posted_by={following}"))
         else:
-            result = db.session.execute(text(f"SELECT id, content FROM messages WHERE posted_by={following} AND is_for_members=False"))
-            count = db.session.execute(text(f"SELECT COUNT(*) FROM messages WHERE posted_by={following} AND is_for_members=False"))
+            result = db.session.execute(text(f"SELECT id, content FROM messages\
+                                              WHERE posted_by={following}\
+                                                  AND is_for_members=False"))
+            count = db.session.execute(text(f"SELECT COUNT(*) FROM messages\
+                                             WHERE posted_by={following}\
+                                                  AND is_for_members=False"))
         messages = result.fetchall()
         amount = count.fetchone()
         return messages, amount
     return render_template("error.html", error="Must be logged in to get followed list!")
 
 #Fetch comments for specific message specified by id
-def get_comments(id:int):
+def get_comments(message_id: int):
     sql = text("SELECT content, posted_by, poster_id FROM comments WHERE source_msg=:source_msg")
-    result = db.session.execute(sql, {"source_msg":id})
+    result = db.session.execute(sql, {"source_msg":message_id})
     comments = result.fetchall()
     return comments
 
@@ -92,9 +105,9 @@ def get_membership():
     return False
 
 #Updates user_log table with user login/logout info.
-def log_user(type:str):
+def log_user(action_type: str):
     user = get_userid()
     now = datetime.now()
     sql = text("INSERT INTO user_log (user_id, action, time) VALUES (:user_id, :action, :time);")
-    db.session.execute(sql, {"user_id":user, "action":type, "time":now})
+    db.session.execute(sql, {"user_id":user, "action":action_type, "time":now})
     db.session.commit()
